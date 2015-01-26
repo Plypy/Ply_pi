376c8708c7391e71a9e8d538d2c5923e440fae1a
Ply_py <me@plypy.com>
2015-01-26 16:58:12 +0800

    update for ok05

--- source/main.s
+++ source/main.s
@@ -19,24 +19,33 @@ main:
         .unreq pinNum
         .unreq pinFunc
 
+/* load the pattern */
+        ptrn .req r4
+        ldr ptrn, =pattern
+        ldr ptrn, [ptrn]
+        mask .req r5
+        mov mask, #1
 loop$:
 
 /* light on it */
         pinNum .req r0
         pinVal .req r1
+/* get info bit */
         mov pinNum, #47
-        mov pinVal, #1
+        mov pinVal, mask
+        and pinVal, ptrn
+
         bl SetGpio
 
         ldr r0, =500000
         bl Wait
 
-/* light off */
-        mov pinNum, #47
-        mov pinVal, #0
-        bl SetGpio
-
-        ldr r0, =500000
-        bl Wait
+        ror mask, #31
 
 b loop$;
+
+/* data */
+        .section .data
+        .align 2
+pattern:
+        .int 0b11111111101010100010001000101010
